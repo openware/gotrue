217e085529feb07feeb5fb489a3394b27ee59749
Maksym Naichuk <mnaichuk@openware.com>
2022-07-20 16:27:04 +0200

    feat(finex): add labels table

---
+++ migrations/20220720000812_create_labels_table.up.sql
@@ -0,0 +1,13 @@
+-- adds lables table 
+
+CREATE TABLE IF NOT EXISTS auth.labels (
+	id          :bigint          NOT NULL
+	user_id     :uuid            NOT NULL
+	label       enum(`email`,`asym-key`,`phone`,`address`,...)      NOT NULL
+	state       enum(`unverified`,`pending`,`verified`,`expired`)   NOT NULL default `unverified`
+	created_at  timestamptz     NOT NULL
+	updated_at  timestamptz     NOT NULL
+	CONSTRAINT labels_pkey PRIMARY KEY (id),
+	CONSTRAINT labels_user_id_fkey FOREIGN KEY (user_id) REFERENCES auth.users(id) ON DELETE CASCADE
+);
+COMMENT ON TABLE auth.labels is 'Auth: Stores labels associated to a user.';
