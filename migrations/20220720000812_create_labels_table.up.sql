77ae9f19c87d7614676518780d4c69f60df618cd
Max Pushkarov <maxpushka200@gmail.com>
2022-07-28 16:01:57 +0300

    fix: invalid syntax in labels SQL migration script

--- migrations/20220720000812_create_labels_table.up.sql
+++ migrations/20220720000812_create_labels_table.up.sql
@@ -1,12 +1,15 @@
 -- adds lables table 
 
+CREATE TYPE label_name AS ENUM ('email','phone','profile','document');
+CREATE TYPE label_state AS ENUM ('unverified','pending','verified','expired');
+
 CREATE TABLE IF NOT EXISTS auth.labels (
-	id          :bigint          NOT NULL
-	user_id     :uuid            NOT NULL
-	label       enum(`email`,`phone`,`profile`,`document`)  NOT NULL
-	state       enum(`unverified`,`pending`,`verified`,`expired`)   NOT NULL default `unverified`
-	created_at  timestamptz     NOT NULL
-	updated_at  timestamptz     NOT NULL
+	id          bigint          NOT NULL,
+	user_id     uuid            NOT NULL,
+	label       label_name  NOT NULL,
+	state       label_state   NOT NULL DEFAULT 'unverified',
+	created_at  timestamptz     NOT NULL,
+	updated_at  timestamptz     NOT NULL,
 	CONSTRAINT labels_pkey PRIMARY KEY (id),
 	CONSTRAINT labels_user_id_fkey FOREIGN KEY (user_id) REFERENCES auth.users(id) ON DELETE CASCADE
 );
