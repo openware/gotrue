007657a45ade16c355ed7c66577997cc6b30cc96
Max Pushkarov <maxpushka200@gmail.com>
2022-09-02 09:24:13 +0300

    test: add more test cases

--- mailer/template_test.go
+++ mailer/template_test.go
@@ -12,13 +12,20 @@ func TestTemplateMailer_ValidateEmail(t *testing.T) {
     IsValid bool
   }{
     { Email: "test@gmail.com", IsValid: true},
+    { Email: "  test@gmail.com", IsValid: true},
+    { Email: "__test@gmail.com__", IsValid: true},
     { Email: "test@openware.com", IsValid: true},
     { Email: "test@relay.firefox.com", IsValid: true},
     { Email: "test@abc", IsValid: true},
     { Email: "test", IsValid: false},
     { Email: "a@b@c@example.com", IsValid: false},
     { Email: "abc.example.com", IsValid: false},
-    { Email: "a@b@c@example.com", IsValid: false},
+    { Email: "@example.com", IsValid: false},
+    { Email: "~@example.com", IsValid: true},
+    { Email: ".@example.com", IsValid: false},
+    { Email: "te st@example.com", IsValid: false},
+    { Email: "customer/department@example.com", IsValid: true},
+    { Email: "$A12345@example.com", IsValid: true},
   }
 
   for _, tt := range testCases {
